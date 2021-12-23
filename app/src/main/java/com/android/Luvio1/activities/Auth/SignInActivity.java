0502b0b071f77b011651123cd9ff943ca8a220af
Kouvuli <Kouvuli@users.noreply.github.com>
2021-12-23 23:57:13 +0700

    .

--- app/src/main/java/com/android/Luvio1/activities/Auth/SignInActivity.java
+++ app/src/main/java/com/android/Luvio1/activities/Auth/SignInActivity.java
@@ -31,13 +31,9 @@ import at.favre.lib.crypto.bcrypt.BCrypt;
 import io.getstream.chat.android.client.ChatClient;
 import io.getstream.chat.android.client.logger.ChatLogLevel;
 import io.getstream.chat.android.client.models.User;
-<<<<<<< HEAD
 
 import io.getstream.chat.android.client.notifications.handler.ChatNotificationHandler;
 
-=======
-import io.getstream.chat.android.client.notifications.handler.ChatNotificationHandler;
->>>>>>> d28b5149ce1cf5f209c15ea364a28491d29a0bde
 import io.getstream.chat.android.client.notifications.handler.NotificationConfig;
 import io.getstream.chat.android.client.notifications.handler.PushDeviceGenerator;
 import io.getstream.chat.android.livedata.ChatDomain;
